813772f76a19f74d8ea3c157f4d9970ac768e3b9
Sedfer <balovilya@mail.ru>
2016-06-10 19:19:33 +0300

    Created MainWindow UI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,6 +3,39 @@
 MainWindow::MainWindow(QWidget *parent)
     : QWidget(parent)
 {
+    hLayoutMain = new QHBoxLayout();
+
+    vLayoutLeft = new QVBoxLayout();
+    vLayoutRight = new QVBoxLayout();
+    hLayoutMain->addLayout(vLayoutLeft, 5);
+    hLayoutMain->addLayout(vLayoutRight, 2);
+
+    // Left layout (chatWindow, textEdit, buttonSend)
+    chatWindow = new QTextEdit();
+    chatWindow->setReadOnly(true);
+    vLayoutLeft->addWidget(chatWindow, 5);
+    // Bottom layout (textEdit, buttonSend)
+    hLayoutBottom = new QHBoxLayout();
+    textEdit = new QPlainTextEdit();
+    buttonSend = new QPushButton("Send");
+    hLayoutBottom->addWidget(textEdit, 5);
+    hLayoutBottom->addWidget(buttonSend, 1);
+    vLayoutLeft->addLayout(hLayoutBottom, 1);
+
+    // Right layout (list, buttons)
+    listUsers = new QListWidget();
+    buttonAdd = new QPushButton("Add...");
+    buttonRemove = new QPushButton("Remove...");
+    buttonLeave = new QPushButton("Leave");
+    buttonNew = new QPushButton("+New");
+    vLayoutRight->addWidget(listUsers, 5);
+    vLayoutRight->addStretch(1);
+    vLayoutRight->addWidget(buttonAdd, 1);
+    vLayoutRight->addWidget(buttonRemove, 1);
+    vLayoutRight->addWidget(buttonLeave, 1);
+    vLayoutRight->addWidget(buttonNew, 1);
+
+    this->setLayout(hLayoutMain);
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -1,15 +1,33 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+#pragma once
 
-#include <QWidget>
+#include <QtWidgets>
 
 class MainWindow : public QWidget
 {
     Q_OBJECT
+private:
+    // Main layout
+    QHBoxLayout *hLayoutMain;
+
+    // Left layout (chatWindow, bottom layout)
+    QVBoxLayout *vLayoutLeft;
+    QTextEdit *chatWindow;
+    // Bottom layout (textEdit, buttonSend)
+    QHBoxLayout *hLayoutBottom;
+    QPlainTextEdit *textEdit;
+    QPushButton *buttonSend;
+
+    // Right layout (list, buttons)
+    QVBoxLayout *vLayoutRight;
+    QListWidget *listUsers;
+    QPushButton *buttonAdd;
+    QPushButton *buttonRemove;
+    QPushButton *buttonLeave;
+    QPushButton *buttonNew;
+
 
 public:
     MainWindow(QWidget *parent = 0);
     ~MainWindow();
 };
 
-#endif // MAINWINDOW_H
